1bb8691d389e9b22da38f4417737ec83e508ded3
SheaClose <SheaClose@gmail.com>
2017-09-18 12:06:25 -0500

    done

--- ctrl.js
+++ ctrl.js
@@ -1,14 +1,14 @@
-angular.module("app").controller("ctrl", function($scope, $timeout){
-	var socket = io();
-	$scope.messages = []
+angular.module('app').controller('ctrl', function($scope, $timeout) {
+  var socket = io();
+  $scope.messages = [];
 
-	$scope.submit = function() {
-		socket.emit('chat message', $scope.inputMessage);
-		$scope.inputMessage = ""
-	}
-	socket.on('chat message', function(msg){
-		$timeout(function(){
-			$scope.messages.push(msg)
-		})
-	})
-})
+  $scope.submit = function(msg, user) {
+    socket.emit('chat message', msg, user);
+    $scope.inputMessage = '';
+  };
+  socket.on('chat message', function(msg, user) {
+    $timeout(function() {
+      $scope.messages.push({ msg, user });
+    });
+  });
+});
--- index.js
+++ index.js
@@ -3,19 +3,18 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use("/", express.static(__dirname));
+app.use('/', express.static(__dirname));
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg, user) {
+    io.emit('chat message', msg, user);
   });
-	socket.on('disconnect', function(){
-		io.emit('user disconnected', {msg:'user disconnected'});
-		console.log('user disconnected');
-	})
+  socket.on('disconnect', function() {
+    io.emit('user disconnected', { msg: 'user disconnected' });
+    console.log('user disconnected');
+  });
 });
 
-
-http.listen(3000, function(){
+http.listen(3000, function() {
   console.log('listening on *:3000');
 });
